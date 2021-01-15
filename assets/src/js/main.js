f1b5d92111d9461970a9c9130389eb2233b5e8ed
Stas <stas@Stass-MacBook-Pro.local>
2021-01-15 20:49:26 +0200

    c6 in f

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -15,7 +15,7 @@
 //= ../../../node_modules/bootstrap/js/dist/tooltip.js
 //= ../../../node_modules/bootstrap/js/dist/popover.js
 //= ../../../node_modules/bootstrap/js/dist/scrollspy.js
-//= ../../../node_modules/bootstrap/js/dist/tab.js
+= ../../../node_modules/bootstrap/js/dist/tab.js
 = ../../../node_modules/bootstrap/js/dist/toast.js
 
 // Импортируем другие js-файлы
