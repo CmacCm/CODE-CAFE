782e2020c3903bcca4b5ba3839afa2273a801cd4
Stas <stas@Stass-MacBook-Pro.local>
2021-01-17 18:00:49 +0200

    test

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -4,19 +4,4 @@
 
 //some change in the same file
 
-//third commit on develop
-
-//fourth commit on develop
-
-//fourth commit after rebase and pull
-
-//fivth commit after master commit and first develop commit
-
-//one more commit 
-
-//one more commit
-
-//git some commit
-
-// another change in another branch
 
